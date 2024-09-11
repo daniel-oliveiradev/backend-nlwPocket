317551ef7e2d03126403129fe1c1a670c17f2612
Daniel Oliveira <daniel.oliveiraprog@gmail.com>
2024-09-11 15:22:13 -0300

    route of 'create-goal' implemented

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import { creatGoal } from '../functions/create-goal'
+import z from 'zod'
 
 const app = fastify()
+
+app.post('/goals', async request => {
+  const creatGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = creatGoalSchema.parse(request.body)
+
+  await creatGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
